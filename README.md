6ddfaf29740bd0415385fe5b0c2093d5693259ae
Michal Klauza <klauza.uk@gmail.com>
2021-04-17 15:07:56 +0100

    archive update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+## About
+
+A simple app to create and delete to-do tasks. The app uses localstorage.
+
+<img src="https://user-images.githubusercontent.com/43997053/115115801-4a8f2680-9f8e-11eb-967f-06162fc99f26.PNG" width="500">
+
+## Installation
+
+project archived, need python2 to run
+
 run **gulp watch** for sass compile and browser-sync<br/>
+
+## Other
+
 > Project inspired by https://www.portexe.com/
--- js/javaScript.js
+++ js/javaScript.js
@@ -1,76 +1,92 @@
-const storageKey = "store_todos"  //can store stirng only, so need 2 functions
+const storageKey = 'store_todos'; // can store string only
 
-//one for converting FROM the storage
-const convertStringToObj = (str) => JSON.parse(str) || []; //we always want an array as return
+// get object from storage and convert to string
+const convertStringToObj = (str) => JSON.parse(str) || []; // array as return
 
-//second for converting TO the storage
-const convertObjToString = (obj) => JSON.stringify(obj) || ""; //we always want a string as return
+// convert object to string and save into storage
+const convertObjToString = (obj) => JSON.stringify(obj) || ''; // string as return
 
 const getTodos = () => convertStringToObj(localStorage.getItem(storageKey));
 
-const addTodo = (todo) => localStorage.setItem(storageKey, convertObjToString([...getTodos(), todo]));
+const addTodo = (todo) =>
+  localStorage.setItem(storageKey, convertObjToString([...getTodos(), todo]));
 
-//same array minus the item we want to delete
-const deleteTodo = (todo) => localStorage.setItem(storageKey, convertObjToString(getTodos().filter(_todo => _todo !== todo)));
+// array minus deleted item
+const deleteTodo = (todo) =>
+  localStorage.setItem(
+    storageKey,
+    convertObjToString(getTodos().filter((_todo) => _todo !== todo))
+  );
 
-//build an element and returns it
+// builds an element and returns it
 const buildTodoEl = (todo) => {
-  const el = document.createElement("li");
-  el.classList.add("list-group-item");
+  const el = document.createElement('li');
+  el.classList.add('list-group-item');
   el.innerText = todo;
   return el;
-}
+};
 
-const appendLiToDom = (el) => document.getElementById("todo-list-container").appendChild(el);
+const appendLiToDom = (el) =>
+  document.getElementById('todo-list-container').appendChild(el);
 
-const clearTodoListDisplay = () => document.getElementById("todo-list-container").innerHTML = "";
+const clearTodoListDisplay = () =>
+  (document.getElementById('todo-list-container').innerHTML = '');
 
-const clearInput = () => document.getElementById("new-todo-input").value = "";
+const clearInput = () => (document.getElementById('new-todo-input').value = '');
 
 const displayTodos = () => {
   clearInput();
   clearTodoListDisplay();
-  getTodos().forEach(_todo => appendLiToDom(buildTodoEl(_todo)));
+  getTodos().forEach((_todo) => appendLiToDom(buildTodoEl(_todo)));
   initClickListeners();
-}
+};
 
 const initClickListeners = () => {
-  Array.from(document.getElementsByClassName("list-group-item")).forEach(_item => {
-    _item.addEventListener("click", ($event) => {
-      const todo = $event.target.innerText;
-      if(window.confirm("Are you sure you want to delete this task? " + todo)){
-        deleteTodo(todo);
-        displayTodos();
-      }
-    });
-  });
-}
+  Array.from(document.getElementsByClassName('list-group-item')).forEach(
+    (_item) => {
+      _item.addEventListener('click', ($event) => {
+        const todo = $event.target.innerText;
+        if (
+          window.confirm('Are you sure you want to delete this task? ' + todo)
+        ) {
+          deleteTodo(todo);
+          displayTodos();
+        }
+      });
+    }
+  );
+};
 
-//display todos and focus input on DOM load
-document.addEventListener("DOMContentLoaded", () => {
+// display todos and focus input on DOM load
+document.addEventListener('DOMContentLoaded', () => {
   displayTodos();
-  document.getElementById("new-todo-input").focus();
-})
-
-//submit button
-document.getElementById("submit-new-todo-btn").addEventListener("click", ($event) => {
-  const newTodoInput = document.getElementById("new-todo-input");
-  if(newTodoInput.value){
-    addTodo(newTodoInput.value.trim()); //trim removes white space
-    displayTodos();
-  }
-})
-
-//submit button on enter hit
-document.getElementById("new-todo-input").addEventListener("keyup", function($event) {
-  if ($event.keyCode === 13) {
-    document.getElementById("submit-new-todo-btn").click();
-  }
+  document.getElementById('new-todo-input').focus();
 });
 
+// submit button
+document
+  .getElementById('submit-new-todo-btn')
+  .addEventListener('click', ($event) => {
+    const newTodoInput = document.getElementById('new-todo-input');
+    if (newTodoInput.value) {
+      addTodo(newTodoInput.value.trim()); //trim removes white space
+      displayTodos();
+    }
+  });
+
+// submit button on enter hit
+document
+  .getElementById('new-todo-input')
+  .addEventListener('keyup', function ($event) {
+    if ($event.keyCode === 13) {
+      document.getElementById('submit-new-todo-btn').click();
+    }
+  });
 
 //reset local storage button
-document.getElementById("reset-storage-button").addEventListener("click", ($event) => {
-  localStorage.removeItem(storageKey);
-  displayTodos();
-})
+document
+  .getElementById('reset-storage-button')
+  .addEventListener('click', ($event) => {
+    localStorage.removeItem(storageKey);
+    displayTodos();
+  });
